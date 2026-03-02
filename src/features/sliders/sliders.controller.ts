93e6686ed3e20f80fdfb595994acb5c3241f1f98
Namory <nnamorykanate5@gmail.com>
2026-03-02 14:00:04 +0100

    Finish slider projet

--- src/features/sliders/sliders.controller.ts
+++ src/features/sliders/sliders.controller.ts
@@ -1,34 +1,75 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, UseInterceptors, BadRequestException, UploadedFiles, ValidationPipe, Query } from '@nestjs/common';
 import { SlidersService } from './sliders.service';
 import { CreateSliderDto } from './dto/create-slider.dto';
 import { UpdateSliderDto } from './dto/update-slider.dto';
+import { ApiConsumes } from '@nestjs/swagger/dist/decorators/api-consumes.decorator';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { memoryStorage } from 'multer';
+import { UserRoles } from 'src/shared/common/user-roles.enum';
+import { AuthGuard } from 'src/shared/guards/auth.guard';
+import { AuthorizeGuard } from 'src/shared/guards/authorization.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthorizeRoles } from 'src/shared/decorators/authorize-roles.decorator';
+import { FormDataTransformPipe } from 'src/shared/pipes/formdata-transform.pipe';
 
+
+@ApiTags('sliders')
+@ApiBearerAuth()
 @Controller('sliders')
 export class SlidersController {
-  constructor(private readonly slidersService: SlidersService) {}
+  constructor(private readonly slidersService: SlidersService) { }
 
+
+  /*
   @Post()
   create(@Body() createSliderDto: CreateSliderDto) {
     return this.slidersService.create(createSliderDto);
-  }
+  }*/
 
   @Get()
   findAll() {
     return this.slidersService.findAll();
   }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.slidersService.findOne(+id);
+  @Get('sliderTop')
+  async findTop(@Query('limit') limit: string) {
+    // On transforme le texte reçu en nombre. 
+    // Si 'limit' n'est pas envoyé, on passe undefined et le service prendra 3 par défaut.
+    const topeSliders = limit ? parseInt(limit, 10) : undefined;
+    return this.slidersService.findTopSliders(topeSliders);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSliderDto: UpdateSliderDto) {
-    return this.slidersService.update(+id, updateSliderDto);
+  @AuthorizeRoles(UserRoles.ADMIN)
+  @UseGuards(AuthGuard, AuthorizeGuard)
+  @ApiConsumes('multipart/form-data')
+  @UseInterceptors(
+    FileFieldsInterceptor([{ name: 'newImage', maxCount: 1 }], {
+      storage: memoryStorage(), // Le fichier n'est pas écrit sur le disque ici
+      limits: { fileSize: 2 * 1024 * 1024 },
+      fileFilter: (req, file, cb) => {
+        if (!file.mimetype.match(/\/(jpg|jpeg|png|webp)$/)) {
+          return cb(new BadRequestException('Format invalide'), false);
+        }
+        cb(null, true);
+      },
+    }),
+  )
+  @Patch(':slug')
+  update(
+    @UploadedFiles()
+    files: {
+      newImage?: Express.Multer.File[];
+    },
+    @Param('slug') slug: string,
+    @Body(FormDataTransformPipe, ValidationPipe)
+    updateSliderDto: UpdateSliderDto) {
+    return this.slidersService.update(slug, updateSliderDto, files);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.slidersService.remove(+id);
+  @AuthorizeRoles(UserRoles.ADMIN)
+  @UseGuards(AuthGuard, AuthorizeGuard)
+  @Delete(':slug')
+  remove(@Param('slug') slug: string) {
+    return this.slidersService.remove(slug);
   }
 }
+
--- src/features/sliders/sliders.service.ts
+++ src/features/sliders/sliders.service.ts
@@ -4,34 +4,177 @@ import { UpdateSliderDto } from './dto/update-slider.dto';
 import { Slider } from './entities/slider.entity';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { ApiResponse } from 'src/shared/responses/api-response';
+import { SlidersModule } from './sliders.module';
+import * as fs from 'fs';
+import * as path from 'path';
 
 @Injectable()
 export class SlidersService {
   constructor(
-    @InjectModel(Slider.name) private readonly userModel: Model<Slider>,
+    @InjectModel(Slider.name) private readonly sliderModel: Model<Slider>,
   ) { }
-  /*
-  create(createSliderDto: CreateSliderDto) {
-    return 'This action adds a new slider';
-  }*/
+async create(
+  createSliderDto: CreateSliderDto,
+  files: { newImage?: Express.Multer.File[] }
+) {
+  const file = files.newImage?.[0];
+  let fullPath = '';
+  let relativePath = '';
 
-  findAll() {
-    try {
+  try {
+    // 1. Vérifications initiales
+    const existingSlider = await this.sliderModel.findOne({ title: createSliderDto.title });
+    if (existingSlider) return ApiResponse.error('Le slider existe déjà');
 
-    } catch (error) {
+    const existingOrder = await this.sliderModel.findOne({ order: createSliderDto.order });
+    if (existingOrder) return ApiResponse.error('Cet ordre est déjà utilisé');
+
+    // 2. Gestion du fichier image
+    if (file) {
+      const uploadDir = './storage/sliders';
+      if (!fs.existsSync(uploadDir)) {
+        fs.mkdirSync(uploadDir, { recursive: true });
+      }
+
+      const uniqueSuffix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
+      const fileName = `slider-${uniqueSuffix}${path.extname(file.originalname)}`;
+      
+      fullPath = path.join(uploadDir, fileName);
+      relativePath = `storage/sliders/${fileName}`;
+
+      fs.writeFileSync(fullPath, file.buffer);
     }
-  }
 
-  findOne(id: number) {
-    return `This action returns a #${id} slider`;
-  }
+    // 3. Création de l'instance et sauvegarde en BDD
+    const newSlider = new this.sliderModel({
+      ...createSliderDto,
+      image: relativePath, // On stocke le chemin relatif
+    });
 
-  update(id: number, updateSliderDto: UpdateSliderDto) {
-    return `This action updates a #${id} slider`;
-  }
+    const savedSlider = await newSlider.save();
 
-  remove(id: number) {
-    return `This action removes a #${id} slider`;
+    return ApiResponse.success('Slider créé avec succès', savedSlider);
+
+  } catch (error) {
+    if (fullPath && fs.existsSync(fullPath)) {
+      fs.unlinkSync(fullPath);
+    }
+    return ApiResponse.error('Erreur lors de la création du slider');
   }
 }
+  //Récuperation des sliders sans order 1
+  async findAll() {
+    try {
+      const sliders = await this.sliderModel.find().sort({ order: -1 }).exec();
+      return ApiResponse.success("Liste des sliders récupérée", sliders);
+    } catch (error) {
+      return ApiResponse.error('Erreur lors de la récupération des sliders');
+    }
+  }
+  // Retoune les sliders avec les orders les plus grand.
+  async findTopSliders(limit: number = 3) {
+    try {
+      const finalLimit = Math.max(1, Math.floor(limit));
+      const topSliders = await this.sliderModel
+        .find()
+        .sort({ order: -1 }) // Trie du plus grand au plus petit
+        .limit(finalLimit)   // Utilise la limite dynamique
+        .exec();
+      return ApiResponse.success(`Top ${finalLimit} sliders récupérés`, topSliders);
+    }
+    catch (error) {
+      return ApiResponse.error("Erreur lors de la récupération des sliders");
+    }
+  }
+  async update(
+    slug: string,
+    updateSliderDto: UpdateSliderDto,
+    files: { newImage?: Express.Multer.File[] },
+  ) {
+    const file = files?.newImage?.[0];
+    let oldImagePath: string | null = null;
+    let newRelativePath: string | null = null;
 
+    try {
+      // 1. Trouver le slider actuel
+      const slider = await this.sliderModel.findOne({ slug });
+      if (!slider) {
+        return ApiResponse.error('Slider introuvable');
+      }
+      // 2. Gestion de la nouvelle image (si présente)
+      if (file) {
+        const uploadDir = './storage/sliders';
+        if (!fs.existsSync(uploadDir)) {
+          fs.mkdirSync(uploadDir, { recursive: true });
+        }
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+        const fileName = `slider-${uniqueSuffix}${path.extname(file.originalname)}`;
+
+        newRelativePath = `storage/sliders/${fileName}`;
+        const fullPath = path.join(uploadDir, fileName);
+        // Écriture du fichier
+        fs.writeFileSync(fullPath, file.buffer);
+
+        // On mémorise l'ancienne image pour la supprimer après le succès en BDD
+        oldImagePath = slider.image;
+
+        // Mise à jour du chemin dans le DTO
+        updateSliderDto.image = newRelativePath;
+      }
+      const updatedSlider = await this.sliderModel.findOneAndUpdate(
+        { slug },
+        { $set: updateSliderDto },
+        { new: true },
+      );
+      if (file && oldImagePath) {
+        const oldFullRootPath = path.join(process.cwd(), oldImagePath);
+        if (fs.existsSync(oldFullRootPath)) {
+          fs.unlinkSync(oldFullRootPath);
+        }
+      }
+      return ApiResponse.success(
+        'Slider mis à jour avec succès',
+        updatedSlider
+      );
+
+    } catch (error) {
+      if (newRelativePath) {
+        const tempPath = path.join(process.cwd(), newRelativePath);
+        if (fs.existsSync(tempPath)) fs.unlinkSync(tempPath);
+      }
+      return ApiResponse.error('Erreur lors de la mise à jour');
+    }
+  }
+  async remove(slug: string) {
+    try {
+      const slider = await this.sliderModel.findOne({ slug });
+      if (!slider) {
+        return ApiResponse.error('Slider introuvable.');
+      }
+
+      // 2. Suppression du fichier physique
+      if (slider.image) {
+        const fullPath = path.join(process.cwd(), slider.image);
+
+        if (fs.existsSync(fullPath)) {
+          try {
+            fs.unlinkSync(fullPath);
+          } catch (fileError) {
+            return ApiResponse.error(
+              'Une erreur est survenue lors de la suppression de l’image', fileError
+            );
+          }
+        }
+      }
+
+      // 3. Suppression en base de données
+      const result = await this.sliderModel.deleteOne({ slug: slug });
+      return ApiResponse.success('Slider supprimé avec succès.');
+    }
+    catch (error) {
+      console.error(error);
+      return ApiResponse.error('Erreur lors de la suppression du slider.');
+    }
+  }
+}
